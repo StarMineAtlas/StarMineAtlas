53e6bfab6f6bf1ecab872c9fe5bb9bf2a29f91b3
Dorian Marques <dorian.marques.2e7@gmail.com>
2026-03-19 16:33:10 +0100

    make variable google sheet id

--- lib/api-endpoints.ts
+++ lib/api-endpoints.ts
@@ -1,6 +1,7 @@
-// Centralise les URLs et endpoints de l'API Google Sheet
+const GOOGLE_SHEET_ID = "1PNafLL179gh80ipoLP1AUhy47_bWTXVBOx2j1kJWd2I";
+const OPENSHEET_BASE_URL = 'https://opensheet.elk.sh/';
 
-export const API_BASE_URL = "https://opensheet.elk.sh/1PNafLL179gh80ipoLP1AUhy47_bWTXVBOx2j1kJWd2I/";
+export const API_BASE_URL = OPENSHEET_BASE_URL + GOOGLE_SHEET_ID + "/";
 export const API_UEX_BASE_URL = "https://api.uexcorp.space/2.0/";
 
 export const API_ENDPOINTS = {
